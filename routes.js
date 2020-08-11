04baba305e830ae0fb56ad56b991284e6138bc5f
Ezequias Aramburu <ezequias@MacBook-Pro-de-Ezequias.local>
2020-08-11 16:41:07 -0300

    add config to allow cors

--- routes.js
+++ routes.js
@@ -4,5 +4,13 @@
 'use strict';
 
 module.exports = function(app) {
+  // Add headers
+  app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  });
   app.use('/', require('./api/scraping/router'));
 };
